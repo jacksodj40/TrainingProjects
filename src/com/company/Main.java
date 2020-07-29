eb696c65a19cb3dbcede424f124c313491069bae
Derek J <jackson.derek40@gmail.com>
2020-07-29 16:54:00 -0700

    First Git Upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import javax.swing.*;
+import javax.xml.namespace.QName;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+
+        Product product = new Product("Keyboard");
+        System.out.println(product.getName());
+
+//        double price;
+//        double expectedDownPayment;
+//        price = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter the price of the house"));
+
+//        expectedDownPayment = price * 0.2;
+//        System.out.println(expectedDownPayment);
+
+
+
+
+    }
+}
